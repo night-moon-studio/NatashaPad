817091957392654082e9e82d5692b9af48663466
dane <838652739@qq.com>
2020-07-30 12:50:23 +0800

    添加对话框基类

---
+++ src/NatashaPad/ViewModels/DialogViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NatashaPad.ViewModels
+{
+    internal abstract class DialogViewModelBase : ViewModelBase
+    {
+        protected DialogViewModelBase(CommonParam commonParam) : base(commonParam)
+        {
+            OkCommand = new DelegateCommand(async () => await OkAsync(), CanOk);
+            CancelCommand = new DelegateCommand(async () => await CancelAsync(), CanCancel);
+        }
+
+        public ICommand OkCommand { get; }
+        protected abstract Task OkAsync();
+        protected virtual bool CanOk() => true;
+
+        public ICommand CancelCommand { get; }
+        protected virtual Task CancelAsync() => Task.CompletedTask;
+        protected virtual bool CanCancel() => true;
+    }
+}
