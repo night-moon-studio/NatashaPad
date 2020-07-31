4526487d2907aa253df21238b9f1a1d2be8bfa90
dane <838652739@qq.com>
2020-07-31 13:08:56 +0800

    feat(基础): 添加基础服务

---
+++ src/NatashaPad.MvvmServices/DefaultViewContainer.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NatashaPad.MvvmServices
+{
+    internal class DefaultViewContainer : IViewContainer, IViewLocator, IViewInstanceLocator
+    {
+        private readonly IServiceCollection services;
+        private readonly Dictionary<Type, Type> map;
+
+        public DefaultViewContainer(IServiceCollection services)
+        {
+            this.services = services;
+
+            map = new Dictionary<Type, Type>();
+        }
+
+        
+
+        public Type GetView<TViewModel>()
+        {
+            if (!map.TryGetValue(typeof(TViewModel), out var viewType))
+            {
+                throw new KeyNotFoundException(
+                    string.Format(Properties.Resource.CannotFindMatchedViewTypeOfFormatString,
+                    typeof(TViewModel).Name));
+            }
+
+            return viewType;
+        }
+
+        public void Register<TView, TViewModel>() where TView:class
+        {
+            Register(typeof(TViewModel), typeof(TView));
+            services.TryAddTransient<TView>();
+        }
+
+        private void Register(Type viewModelType, Type viewType)
+        {
+            map[viewModelType] = viewType;
+        }
+    }
+
+    internal class DefaultViewLocator : IViewInstanceLocator
+    {
+        //public object GetView<TViewModel>(TViewModel vm)
+        //{
+        //    if (!map.TryGetValue(typeof(TViewModel), out var viewType))
+        //    {
+        //        throw new KeyNotFoundException(
+        //            string.Format(Properties.Resource.CannotFindMatchedViewTypeOfFormatString,
+        //            typeof(TViewModel).Name));
+        //    }
+
+        //    return serviceProvider.GetService(viewType);
+        //}
+    }
+}
---
+++ src/NatashaPad.MvvmServices/IViewContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NatashaPad.MvvmServices
+{
+    public interface IViewContainer
+    {
+        void Register<TView, TViewModel>() where TView : class;
+    }
+
+    internal interface IViewLocator
+    {
+        Type GetView<TViewModel>();
+    }
+
+    public interface IViewInstanceLocator
+    {
+        object GetView<TViewModel>(TViewModel vm);
+    }
+}
