b7e12072bd54ebe0fbe8a7e07f99d73626163575
dane <838652739@qq.com>
2020-08-04 12:38:07 +0800

    feat(基础): 处理request

---
+++ src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using MediatR;
+
+namespace NatashaPad.MvvmServices.ViewRequests
+{
+    public class ShowViewRequest : IRequest
+    {
+        public ShowViewRequest(object vm, Type type)
+        {
+            ViewModel = vm;
+            Type = type;
+        }
+
+        public object ViewModel { get; }
+        public Type Type { get; }
+
+        public static ShowViewRequest Create<TViewModel>(TViewModel vm)
+        {
+            return new ShowViewRequest(vm, typeof(TViewModel));
+        }
+    }
+}
---
+++ src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+using MediatR;
+
+namespace NatashaPad.MvvmServices.ViewRequests
+{
+    public class ShowViewRequestHandler : RequestHandler<ShowViewRequest>
+    {
+        private readonly IViewInstanceLocator locator;
+
+        public ShowViewRequestHandler(IViewInstanceLocator locator)
+        {
+            this.locator = locator;
+        }
+
+        protected override void Handle(ShowViewRequest request)
+        {
+            var window = new Window();
+            window.Content = locator.GetView(request.Type);
+            window.DataContext = request.ViewModel;
+            window.ShowDialog();
+        }
+    }
+}
