6c8559ae1bcce4546b135ad3efe45ed83be57747
dane <838652739@qq.com>
2020-08-04 13:03:19 +0800

    feat(基础): 内容自适应

--- src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequestHandler.cs
+++ src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequestHandler.cs
@@ -18,7 +18,16 @@ namespace NatashaPad.MvvmServices.ViewRequests
             var window = new Window();
             window.Content = locator.GetView(request.Type);
             window.DataContext = request.ViewModel;
+            window.SizeToContent = SizeToContent.WidthAndHeight;
             window.ShowDialog();
+
+            /*
+             * 可选的解法：
+             * 在资源文件中定义View和ViewModel的映射关系
+             * 预先设计好默认window
+             * window中的内容元素的内容绑定到DataContext
+             * 这样，就不需要在这个方法里硬编码，把扩展性开放出去
+             */
         }
     }
 }
