697c74e03abfa2ab8d6ab87456f18d123a11350a
dane <838652739@qq.com>
2020-08-05 13:01:59 +0800

    feat(基础): 添加接口

---
+++ src/NatashaPad.MvvmServices/IWindowService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NatashaPad.MvvmServices
+{
+    public interface IWindowService
+    {
+        TViewModel Show<TViewModel>();
+    }
+
+    public interface IDialogService : IWindowService
+    {
+        TViewModel ShowDialog<TViewModel>();
+    }
+}
