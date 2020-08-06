e20753ffed47c66684a40eff798a093f8026268e
dane <838652739@qq.com>
2020-08-06 13:02:45 +0800

    feat(service): 修改注释

--- src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequestHandler.cs
+++ src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequestHandler.cs
@@ -21,11 +21,12 @@ namespace NatashaPad.MvvmServices.ViewRequests
             window.SizeToContent = SizeToContent.WidthAndHeight;
             window.ShowDialog();
 
-            /*
-             * 可选的解法：
+            /* 
+             * TODO: 可选的解法：
              * 在资源文件中定义View和ViewModel的映射关系
              * 预先设计好默认window
              * window中的内容元素的内容绑定到DataContext
+             * 注册时，提供name，窗口尺寸等其它信息
              * 这样，就不需要在这个方法里硬编码，把扩展性开放出去
              */
         }
