27198db50c7f572dda1bbb14d74aeb70336a30f2
dane <838652739@qq.com>
2020-08-07 13:08:07 +0800

    feat(service): 注释不使用的接口文件

--- src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequest.cs
+++ src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequest.cs
@@ -1,23 +1,23 @@
-﻿using System;
+﻿//using System;
 
-using MediatR;
+//using MediatR;
 
-namespace NatashaPad.MvvmServices.ViewRequests
-{
-    public class ShowViewRequest : IRequest
-    {
-        public ShowViewRequest(object vm, Type type)
-        {
-            ViewModel = vm;
-            Type = type;
-        }
+//namespace NatashaPad.MvvmServices.ViewRequests
+//{
+//    public class ShowViewRequest : IRequest
+//    {
+//        public ShowViewRequest(object vm, Type type)
+//        {
+//            ViewModel = vm;
+//            Type = type;
+//        }
 
-        public object ViewModel { get; }
-        public Type Type { get; }
+//        public object ViewModel { get; }
+//        public Type Type { get; }
 
-        public static ShowViewRequest Create<TViewModel>(TViewModel vm)
-        {
-            return new ShowViewRequest(vm, typeof(TViewModel));
-        }
-    }
-}
+//        public static ShowViewRequest Create<TViewModel>(TViewModel vm)
+//        {
+//            return new ShowViewRequest(vm, typeof(TViewModel));
+//        }
+//    }
+//}
--- src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequestHandler.cs
+++ src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequestHandler.cs
@@ -1,29 +1,29 @@
-﻿using System.Windows;
+﻿//using System.Windows;
 
-using MediatR;
+//using MediatR;
 
-using NatashaPad.MvvmServices.Windows;
+//using NatashaPad.MvvmServices.Windows;
 
-namespace NatashaPad.MvvmServices.ViewRequests
-{
-    public class ShowViewRequestHandler : RequestHandler<ShowViewRequest>
-    {
-        private readonly IViewInstanceLocator locator;
-        private readonly IWindowProvider windowProvider;
+//namespace NatashaPad.MvvmServices.ViewRequests
+//{
+//    public class ShowViewRequestHandler : RequestHandler<ShowViewRequest>
+//    {
+//        private readonly IViewInstanceLocator locator;
+//        private readonly IWindowProvider windowProvider;
 
-        public ShowViewRequestHandler(IViewInstanceLocator locator,
-            IWindowProvider windowProvider)
-        {
-            this.locator = locator;
-            this.windowProvider = windowProvider;
-        }
+//        public ShowViewRequestHandler(IViewInstanceLocator locator,
+//            IWindowProvider windowProvider)
+//        {
+//            this.locator = locator;
+//            this.windowProvider = windowProvider;
+//        }
 
-        protected override void Handle(ShowViewRequest request)
-        {
-            var view = locator.GetView(request.Type);
-            var viewModel = request.ViewModel;
-            var window = windowProvider.Create(view, viewModel);
-            window.ShowDialog();
-        }
-    }
-}
+//        protected override void Handle(ShowViewRequest request)
+//        {
+//            var view = locator.GetView(request.Type);
+//            var viewModel = request.ViewModel;
+//            var window = windowProvider.Create(view, viewModel);
+//            window.ShowDialog();
+//        }
+//    }
+//}
