a0f29460881c631087fab2056676de0e6dee0d6e
dane <838652739@qq.com>
2020-08-07 12:29:42 +0800

    feat(service): 使用窗口服务创建窗口

--- src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequestHandler.cs
+++ src/NatashaPad.MvvmServices/ViewRequests/ShowViewRequestHandler.cs
@@ -2,33 +2,28 @@
 
 using MediatR;
 
+using NatashaPad.MvvmServices.Windows;
+
 namespace NatashaPad.MvvmServices.ViewRequests
 {
     public class ShowViewRequestHandler : RequestHandler<ShowViewRequest>
     {
         private readonly IViewInstanceLocator locator;
+        private readonly IWindowProvider windowProvider;
 
-        public ShowViewRequestHandler(IViewInstanceLocator locator)
+        public ShowViewRequestHandler(IViewInstanceLocator locator,
+            IWindowProvider windowProvider)
         {
             this.locator = locator;
+            this.windowProvider = windowProvider;
         }
 
         protected override void Handle(ShowViewRequest request)
         {
-            var window = new Window();
-            window.Content = locator.GetView(request.Type);
-            window.DataContext = request.ViewModel;
-            window.SizeToContent = SizeToContent.WidthAndHeight;
+            var view = locator.GetView(request.Type);
+            var viewModel = request.ViewModel;
+            var window = windowProvider.Create(view, viewModel);
             window.ShowDialog();
-
-            /* 
-             * TODO: 可选的解法：
-             * 在资源文件中定义View和ViewModel的映射关系
-             * 预先设计好默认window
-             * window中的内容元素的内容绑定到DataContext
-             * 注册时，提供name，窗口尺寸等其它信息
-             * 这样，就不需要在这个方法里硬编码，把扩展性开放出去
-             */
         }
     }
 }
