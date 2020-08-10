62099ae1f3e5fae8a41b36f682b740e06afc38e1
dane <838652739@qq.com>
2020-08-10 12:39:26 +0800

    Revert "feat(service): 支持window的定位"
    This reverts commit 64bf8d033b76aea01470afee66a104a5a26bbfa6.

--- src/NatashaPad.MvvmServices/Windows/DefaultWindowManager.cs
+++ src/NatashaPad.MvvmServices/Windows/DefaultWindowManager.cs
@@ -7,7 +7,7 @@ using MediatR;
 
 namespace NatashaPad.MvvmServices.Windows
 {
-    public class DefaultWindowManager : IWindowManager
+    internal class DefaultWindowManager : IWindowManager
     {
         private readonly IViewInstanceLocator locator;
         private readonly IWindowProvider windowProvider;
@@ -36,12 +36,8 @@ namespace NatashaPad.MvvmServices.Windows
         public IDialogService GetDialogService<TViewModel>(TViewModel viewModel)
         {
             var view = locator.GetView(typeof(TViewModel));
-
-            if (!(view is Window window))
-            {
-                window = windowProvider.Create(view, viewModel);
-                window.Closed += Window_Closed;
-            }
+            var window = windowProvider.Create(view, viewModel);
+            window.Closed += Window_Closed;
 
             windowMap[viewModel] = window;
             return new DefaultDialogService(window);
