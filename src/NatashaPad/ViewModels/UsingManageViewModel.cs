5217eb64c47a9df136f866c75d99fd492a7ed727
dane <838652739@qq.com>
2020-08-11 13:01:31 +0800

    feat(using): 校验空数据和重复数据

--- src/NatashaPad/ViewModels/UsingManageViewModel.cs
+++ src/NatashaPad/ViewModels/UsingManageViewModel.cs
@@ -7,9 +7,12 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+
+using WeihanLi.Extensions;
 
 namespace NatashaPad.ViewModels
 {
@@ -19,10 +22,8 @@ namespace NatashaPad.ViewModels
             IEnumerable<string> namespaces) : base(commonParam)
         {
             AllItems = new RemovableCollection<NamespaceItem>();
-            foreach (var name in namespaces)
-            {
-                AllItems.Add(new NamespaceItem(name));
-            }
+            namespaces.Distinct()
+                .ForEach(x => AllItems.Add(new NamespaceItem(x)));
 
             AddCommand = new DelegateCommand(Add);
         }
@@ -30,10 +31,23 @@ namespace NatashaPad.ViewModels
         public ICommand AddCommand { get; }
         private void Add()
         {
-            AllItems.Add(new NamespaceItem());
+            if (!AllItems.Any(x => x.IsEmpty))
+                AllItems.Add(new NamespaceItem());
         }
 
         public RemovableCollection<NamespaceItem> AllItems { get; }
+
+        protected override Task OkAsync()
+        {
+            var empties = AllItems.Where(x => x.IsEmpty);
+            var duplicates = AllItems.GroupBy(x => x.Namespace)
+                .SelectMany(x => x.Skip(1));
+
+            empties.Concat(duplicates)
+                .ToArray()
+                .ForEach(x => AllItems.Remove(x));
+            return base.OkAsync();
+        }
     }
 
     internal class NamespaceItem : CollectionItem
@@ -50,6 +64,19 @@ namespace NatashaPad.ViewModels
         {
             get => _namespace;
             set => SetProperty(ref _namespace, value);
+        }
+
+        internal bool IsEmpty => string.IsNullOrWhiteSpace(_namespace);
+
+        public override bool Equals(object obj)
+        {
+            return obj is NamespaceItem item &&
+                   _namespace == item._namespace;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_namespace);
         }
     }
 }
