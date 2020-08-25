8f1f25ee861a03af81ae600be323f01f3d65d9f6
dane <838652739@qq.com>
2020-08-25 12:29:56 +0800

    feat(resource): 字符串

--- src/NatashaPad/Properties/Resource.Designer.cs
+++ src/NatashaPad/Properties/Resource.Designer.cs
@@ -22,7 +22,7 @@ namespace NatashaPad.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resource {
+    public class Resource {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace NatashaPad.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NatashaPad.Properties.Resource", typeof(Resource).Assembly);
@@ -51,7 +51,7 @@ namespace NatashaPad.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,9 +63,27 @@ namespace NatashaPad.Properties {
         /// <summary>
         ///   Looks up a localized string similar to 执行发生异常.
         /// </summary>
-        internal static string ErrorMessageBoxTitleString {
+        public static string ErrorMessageBoxTitleString {
             get {
                 return ResourceManager.GetString("ErrorMessageBoxTitleString", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 引用管理.
+        /// </summary>
+        public static string NugetManageTitleString {
+            get {
+                return ResourceManager.GetString("NugetManageTitleString", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Using管理.
+        /// </summary>
+        public static string UsingManageTitleString {
+            get {
+                return ResourceManager.GetString("UsingManageTitleString", resourceCulture);
             }
         }
     }
